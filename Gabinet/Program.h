d073542c81bc28796f890610ca10b2d1f8aa9e03
Maciek <Maciek@PC>
2017-05-26 07:00:24 +0200

    Praca z kontrolkami

--- Gabinet/Program.h
+++ Gabinet/Program.h
@@ -55,6 +55,31 @@ namespace Gabinet {
 	private: System::Windows::Forms::Button^  btnPDodaj;
 	private: System::Windows::Forms::Button^  btnPModyfikuj;
 	private: System::Windows::Forms::Button^  btnPUsun;
+	private: System::Windows::Forms::GroupBox^  gbGodziny;
+
+	private: System::Windows::Forms::GroupBox^  groupBox4;
+	private: System::Windows::Forms::Button^  button4;
+	private: System::Windows::Forms::Button^  button3;
+	private: System::Windows::Forms::Button^  button2;
+	private: System::Windows::Forms::Button^  button1;
+	private: System::Windows::Forms::TextBox^  textBox11;
+	private: System::Windows::Forms::TextBox^  textBox12;
+	private: System::Windows::Forms::Label^  label13;
+	private: System::Windows::Forms::TextBox^  textBox9;
+	private: System::Windows::Forms::TextBox^  textBox10;
+	private: System::Windows::Forms::Label^  label12;
+	private: System::Windows::Forms::TextBox^  textBox7;
+	private: System::Windows::Forms::TextBox^  textBox8;
+	private: System::Windows::Forms::Label^  label11;
+	private: System::Windows::Forms::TextBox^  textBox5;
+	private: System::Windows::Forms::TextBox^  textBox6;
+	private: System::Windows::Forms::Label^  label10;
+	private: System::Windows::Forms::TextBox^  textBox3;
+	private: System::Windows::Forms::TextBox^  textBox4;
+	private: System::Windows::Forms::Label^  label9;
+	private: System::Windows::Forms::TextBox^  textBox2;
+	private: System::Windows::Forms::TextBox^  textBox1;
+	private: System::Windows::Forms::Label^  label8;
 	public:
 		String^ konfiguracja = L"datasource=localhost;port=3306;username=root;password=kolanko7;database=gabinet";
 		Program(int uzytkownik)
@@ -91,6 +116,30 @@ namespace Gabinet {
 		{
 			this->tabControl1 = (gcnew System::Windows::Forms::TabControl());
 			this->s = (gcnew System::Windows::Forms::TabPage());
+			this->gbGodziny = (gcnew System::Windows::Forms::GroupBox());
+			this->groupBox4 = (gcnew System::Windows::Forms::GroupBox());
+			this->button4 = (gcnew System::Windows::Forms::Button());
+			this->button3 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->textBox11 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox12 = (gcnew System::Windows::Forms::TextBox());
+			this->label13 = (gcnew System::Windows::Forms::Label());
+			this->textBox9 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox10 = (gcnew System::Windows::Forms::TextBox());
+			this->label12 = (gcnew System::Windows::Forms::Label());
+			this->textBox7 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox8 = (gcnew System::Windows::Forms::TextBox());
+			this->label11 = (gcnew System::Windows::Forms::Label());
+			this->textBox5 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox6 = (gcnew System::Windows::Forms::TextBox());
+			this->label10 = (gcnew System::Windows::Forms::Label());
+			this->textBox3 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox4 = (gcnew System::Windows::Forms::TextBox());
+			this->label9 = (gcnew System::Windows::Forms::Label());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->label8 = (gcnew System::Windows::Forms::Label());
 			this->btnPDodaj = (gcnew System::Windows::Forms::Button());
 			this->btnPModyfikuj = (gcnew System::Windows::Forms::Button());
 			this->btnPUsun = (gcnew System::Windows::Forms::Button());
@@ -117,6 +166,8 @@ namespace Gabinet {
 			this->label3 = (gcnew System::Windows::Forms::Label());
 			this->tabControl1->SuspendLayout();
 			this->s->SuspendLayout();
+			this->gbGodziny->SuspendLayout();
+			this->groupBox4->SuspendLayout();
 			this->groupBox2->SuspendLayout();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dgUzytkownicy))->BeginInit();
 			this->tabPage2->SuspendLayout();
@@ -131,11 +182,12 @@ namespace Gabinet {
 			this->tabControl1->Location = System::Drawing::Point(29, 62);
 			this->tabControl1->Name = L"tabControl1";
 			this->tabControl1->SelectedIndex = 0;
-			this->tabControl1->Size = System::Drawing::Size(803, 445);
+			this->tabControl1->Size = System::Drawing::Size(803, 558);
 			this->tabControl1->TabIndex = 0;
 			// 
 			// s
 			// 
+			this->s->Controls->Add(this->gbGodziny);
 			this->s->Controls->Add(this->btnPDodaj);
 			this->s->Controls->Add(this->btnPModyfikuj);
 			this->s->Controls->Add(this->btnPUsun);
@@ -147,14 +199,230 @@ namespace Gabinet {
 			this->s->Location = System::Drawing::Point(4, 4);
 			this->s->Name = L"s";
 			this->s->Padding = System::Windows::Forms::Padding(3);
-			this->s->Size = System::Drawing::Size(795, 419);
+			this->s->Size = System::Drawing::Size(795, 532);
 			this->s->TabIndex = 0;
 			this->s->Text = L"Pracownicy";
 			this->s->UseVisualStyleBackColor = true;
 			// 
+			// gbGodziny
+			// 
+			this->gbGodziny->Controls->Add(this->groupBox4);
+			this->gbGodziny->Controls->Add(this->textBox11);
+			this->gbGodziny->Controls->Add(this->textBox12);
+			this->gbGodziny->Controls->Add(this->label13);
+			this->gbGodziny->Controls->Add(this->textBox9);
+			this->gbGodziny->Controls->Add(this->textBox10);
+			this->gbGodziny->Controls->Add(this->label12);
+			this->gbGodziny->Controls->Add(this->textBox7);
+			this->gbGodziny->Controls->Add(this->textBox8);
+			this->gbGodziny->Controls->Add(this->label11);
+			this->gbGodziny->Controls->Add(this->textBox5);
+			this->gbGodziny->Controls->Add(this->textBox6);
+			this->gbGodziny->Controls->Add(this->label10);
+			this->gbGodziny->Controls->Add(this->textBox3);
+			this->gbGodziny->Controls->Add(this->textBox4);
+			this->gbGodziny->Controls->Add(this->label9);
+			this->gbGodziny->Controls->Add(this->textBox2);
+			this->gbGodziny->Controls->Add(this->textBox1);
+			this->gbGodziny->Controls->Add(this->label8);
+			this->gbGodziny->Location = System::Drawing::Point(37, 192);
+			this->gbGodziny->Name = L"gbGodziny";
+			this->gbGodziny->Size = System::Drawing::Size(304, 294);
+			this->gbGodziny->TabIndex = 8;
+			this->gbGodziny->TabStop = false;
+			this->gbGodziny->Text = L"Godziny pracy";
+			this->gbGodziny->Visible = false;
+			// 
+			// groupBox4
+			// 
+			this->groupBox4->Controls->Add(this->button4);
+			this->groupBox4->Controls->Add(this->button3);
+			this->groupBox4->Controls->Add(this->button2);
+			this->groupBox4->Controls->Add(this->button1);
+			this->groupBox4->Location = System::Drawing::Point(193, 19);
+			this->groupBox4->Name = L"groupBox4";
+			this->groupBox4->Size = System::Drawing::Size(94, 154);
+			this->groupBox4->TabIndex = 18;
+			this->groupBox4->TabStop = false;
+			this->groupBox4->Text = L"Szablony";
+			// 
+			// button4
+			// 
+			this->button4->Location = System::Drawing::Point(6, 113);
+			this->button4->Name = L"button4";
+			this->button4->Size = System::Drawing::Size(75, 23);
+			this->button4->TabIndex = 3;
+			this->button4->Text = L"10-18";
+			this->button4->UseVisualStyleBackColor = true;
+			// 
+			// button3
+			// 
+			this->button3->Location = System::Drawing::Point(6, 83);
+			this->button3->Name = L"button3";
+			this->button3->Size = System::Drawing::Size(75, 23);
+			this->button3->TabIndex = 2;
+			this->button3->Text = L"9-17";
+			this->button3->UseVisualStyleBackColor = true;
+			// 
+			// button2
+			// 
+			this->button2->Location = System::Drawing::Point(6, 53);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(75, 23);
+			this->button2->TabIndex = 1;
+			this->button2->Text = L"8-16";
+			this->button2->UseVisualStyleBackColor = true;
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(6, 23);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(75, 23);
+			this->button1->TabIndex = 0;
+			this->button1->Text = L"7-15";
+			this->button1->UseVisualStyleBackColor = true;
+			// 
+			// textBox11
+			// 
+			this->textBox11->Location = System::Drawing::Point(131, 153);
+			this->textBox11->Name = L"textBox11";
+			this->textBox11->Size = System::Drawing::Size(45, 20);
+			this->textBox11->TabIndex = 17;
+			// 
+			// textBox12
+			// 
+			this->textBox12->Location = System::Drawing::Point(80, 153);
+			this->textBox12->Name = L"textBox12";
+			this->textBox12->Size = System::Drawing::Size(45, 20);
+			this->textBox12->TabIndex = 16;
+			// 
+			// label13
+			// 
+			this->label13->AutoSize = true;
+			this->label13->Location = System::Drawing::Point(39, 156);
+			this->label13->Name = L"label13";
+			this->label13->Size = System::Drawing::Size(41, 13);
+			this->label13->TabIndex = 15;
+			this->label13->Text = L"Sobota";
+			// 
+			// textBox9
+			// 
+			this->textBox9->Location = System::Drawing::Point(131, 127);
+			this->textBox9->Name = L"textBox9";
+			this->textBox9->Size = System::Drawing::Size(45, 20);
+			this->textBox9->TabIndex = 14;
+			// 
+			// textBox10
+			// 
+			this->textBox10->Location = System::Drawing::Point(80, 127);
+			this->textBox10->Name = L"textBox10";
+			this->textBox10->Size = System::Drawing::Size(45, 20);
+			this->textBox10->TabIndex = 13;
+			// 
+			// label12
+			// 
+			this->label12->AutoSize = true;
+			this->label12->Location = System::Drawing::Point(43, 130);
+			this->label12->Name = L"label12";
+			this->label12->Size = System::Drawing::Size(37, 13);
+			this->label12->TabIndex = 12;
+			this->label12->Text = L"Piątek";
+			// 
+			// textBox7
+			// 
+			this->textBox7->Location = System::Drawing::Point(131, 101);
+			this->textBox7->Name = L"textBox7";
+			this->textBox7->Size = System::Drawing::Size(45, 20);
+			this->textBox7->TabIndex = 11;
+			// 
+			// textBox8
+			// 
+			this->textBox8->Location = System::Drawing::Point(80, 101);
+			this->textBox8->Name = L"textBox8";
+			this->textBox8->Size = System::Drawing::Size(45, 20);
+			this->textBox8->TabIndex = 10;
+			// 
+			// label11
+			// 
+			this->label11->AutoSize = true;
+			this->label11->Location = System::Drawing::Point(29, 104);
+			this->label11->Name = L"label11";
+			this->label11->Size = System::Drawing::Size(51, 13);
+			this->label11->TabIndex = 9;
+			this->label11->Text = L"Czwartek";
+			// 
+			// textBox5
+			// 
+			this->textBox5->Location = System::Drawing::Point(131, 75);
+			this->textBox5->Name = L"textBox5";
+			this->textBox5->Size = System::Drawing::Size(45, 20);
+			this->textBox5->TabIndex = 8;
+			// 
+			// textBox6
+			// 
+			this->textBox6->Location = System::Drawing::Point(80, 75);
+			this->textBox6->Name = L"textBox6";
+			this->textBox6->Size = System::Drawing::Size(45, 20);
+			this->textBox6->TabIndex = 7;
+			// 
+			// label10
+			// 
+			this->label10->AutoSize = true;
+			this->label10->Location = System::Drawing::Point(45, 78);
+			this->label10->Name = L"label10";
+			this->label10->Size = System::Drawing::Size(35, 13);
+			this->label10->TabIndex = 6;
+			this->label10->Text = L"Środa";
+			// 
+			// textBox3
+			// 
+			this->textBox3->Location = System::Drawing::Point(131, 49);
+			this->textBox3->Name = L"textBox3";
+			this->textBox3->Size = System::Drawing::Size(45, 20);
+			this->textBox3->TabIndex = 5;
+			// 
+			// textBox4
+			// 
+			this->textBox4->Location = System::Drawing::Point(80, 49);
+			this->textBox4->Name = L"textBox4";
+			this->textBox4->Size = System::Drawing::Size(45, 20);
+			this->textBox4->TabIndex = 4;
+			// 
+			// label9
+			// 
+			this->label9->AutoSize = true;
+			this->label9->Location = System::Drawing::Point(38, 52);
+			this->label9->Name = L"label9";
+			this->label9->Size = System::Drawing::Size(42, 13);
+			this->label9->TabIndex = 3;
+			this->label9->Text = L"Wtorek";
+			// 
+			// textBox2
+			// 
+			this->textBox2->Location = System::Drawing::Point(131, 23);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(45, 20);
+			this->textBox2->TabIndex = 2;
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(80, 23);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(45, 20);
+			this->textBox1->TabIndex = 1;
+			// 
+			// label8
+			// 
+			this->label8->AutoSize = true;
+			this->label8->Location = System::Drawing::Point(11, 26);
+			this->label8->Name = L"label8";
+			this->label8->Size = System::Drawing::Size(69, 13);
+			this->label8->TabIndex = 0;
+			this->label8->Text = L"Poniedziałek";
+			// 
 			// btnPDodaj
 			// 
-			this->btnPDodaj->Location = System::Drawing::Point(265, 332);
+			this->btnPDodaj->Location = System::Drawing::Point(265, 492);
 			this->btnPDodaj->Name = L"btnPDodaj";
 			this->btnPDodaj->Size = System::Drawing::Size(75, 23);
 			this->btnPDodaj->TabIndex = 7;
@@ -165,7 +433,7 @@ namespace Gabinet {
 			// btnPModyfikuj
 			// 
 			this->btnPModyfikuj->Enabled = false;
-			this->btnPModyfikuj->Location = System::Drawing::Point(151, 332);
+			this->btnPModyfikuj->Location = System::Drawing::Point(151, 492);
 			this->btnPModyfikuj->Name = L"btnPModyfikuj";
 			this->btnPModyfikuj->Size = System::Drawing::Size(75, 23);
 			this->btnPModyfikuj->TabIndex = 6;
@@ -176,7 +444,7 @@ namespace Gabinet {
 			// btnPUsun
 			// 
 			this->btnPUsun->Enabled = false;
-			this->btnPUsun->Location = System::Drawing::Point(37, 332);
+			this->btnPUsun->Location = System::Drawing::Point(37, 492);
 			this->btnPUsun->Name = L"btnPUsun";
 			this->btnPUsun->Size = System::Drawing::Size(75, 23);
 			this->btnPUsun->TabIndex = 5;
@@ -193,9 +461,9 @@ namespace Gabinet {
 			this->groupBox2->Controls->Add(this->txtPImie);
 			this->groupBox2->Controls->Add(this->label5);
 			this->groupBox2->Controls->Add(this->chbPPracownik);
-			this->groupBox2->Location = System::Drawing::Point(37, 87);
+			this->groupBox2->Location = System::Drawing::Point(37, 52);
 			this->groupBox2->Name = L"groupBox2";
-			this->groupBox2->Size = System::Drawing::Size(304, 171);
+			this->groupBox2->Size = System::Drawing::Size(304, 120);
 			this->groupBox2->TabIndex = 4;
 			this->groupBox2->TabStop = false;
 			this->groupBox2->Text = L"Edycja danych użytkownika";
@@ -257,6 +525,7 @@ namespace Gabinet {
 			this->chbPPracownik->TabIndex = 0;
 			this->chbPPracownik->Text = L"Pracownik";
 			this->chbPPracownik->UseVisualStyleBackColor = true;
+			this->chbPPracownik->CheckedChanged += gcnew System::EventHandler(this, &Program::chbPPracownik_CheckedChanged);
 			// 
 			// btnPSzukaj
 			// 
@@ -290,7 +559,7 @@ namespace Gabinet {
 			this->dgUzytkownicy->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
 			this->dgUzytkownicy->Location = System::Drawing::Point(357, 19);
 			this->dgUzytkownicy->Name = L"dgUzytkownicy";
-			this->dgUzytkownicy->Size = System::Drawing::Size(420, 383);
+			this->dgUzytkownicy->Size = System::Drawing::Size(420, 496);
 			this->dgUzytkownicy->TabIndex = 0;
 			this->dgUzytkownicy->CellClick += gcnew System::Windows::Forms::DataGridViewCellEventHandler(this, &Program::dgUzytkownicy_CellClick);
 			// 
@@ -300,7 +569,7 @@ namespace Gabinet {
 			this->tabPage2->Location = System::Drawing::Point(4, 4);
 			this->tabPage2->Name = L"tabPage2";
 			this->tabPage2->Padding = System::Windows::Forms::Padding(3);
-			this->tabPage2->Size = System::Drawing::Size(795, 419);
+			this->tabPage2->Size = System::Drawing::Size(795, 532);
 			this->tabPage2->TabIndex = 1;
 			this->tabPage2->Text = L"Zmiana hasła";
 			this->tabPage2->UseVisualStyleBackColor = true;
@@ -387,13 +656,16 @@ namespace Gabinet {
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(871, 541);
+			this->ClientSize = System::Drawing::Size(871, 632);
 			this->Controls->Add(this->tabControl1);
 			this->Name = L"Program";
 			this->Text = L"Program";
 			this->tabControl1->ResumeLayout(false);
 			this->s->ResumeLayout(false);
 			this->s->PerformLayout();
+			this->gbGodziny->ResumeLayout(false);
+			this->gbGodziny->PerformLayout();
+			this->groupBox4->ResumeLayout(false);
 			this->groupBox2->ResumeLayout(false);
 			this->groupBox2->PerformLayout();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dgUzytkownicy))->EndInit();
@@ -608,6 +880,14 @@ private: System::Void btnPUsun_Click(System::Object^  sender, System::EventArgs^
 	chbPPracownik->Checked = false;
 
 	pokaz_siatke();
+}
+private: System::Void chbPPracownik_CheckedChanged(System::Object^  sender, System::EventArgs^  e) {
+	if (chbPPracownik->Checked) {
+		gbGodziny->Visible = true;
+	}
+	else{
+		gbGodziny->Visible = false;
+	}
 }
 };
 }
